38a8108dbe88c95f69c61a805b5a79318f4c0a80
Jed Flynn <jed.flynn.09@gmail.com>
2021-02-22 20:48:45 -0700

    added blog edit authorization

--- src/components/app.js
+++ src/components/app.js
@@ -114,7 +114,12 @@ export default class App extends Component {
                                     <Blog {...props} loggedInStatus={this.state.loggedInStatus} />
                                 )}
                             />
-                            <Route path="/b/:slug" component={BlogDetail} />
+                            <Route
+                                path="/b/:slug"
+                                render={props => (
+                                    <BlogDetail {...props} loggedInStatus={this.state.loggedInStatus} />
+                                )}
+                            />
                             <Route path="/portfolio/:slug" component={PortfolioDetail} />
                             {this.state.loggedInStatus === "LOGGED_IN" ? this.authorizedPages() : null}
                             <Route component={NoMatch} />
